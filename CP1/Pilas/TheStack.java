a875f86031f5dd90eb5a2ce8f7bb6ff86eedb7fd
piero1205 <pjcarva12@gmail.com>
2025-10-27 17:45:09 -0600

    Refactor TheStack class for improved functionality

--- CP1/Pilas/TheStack.java
+++ CP1/Pilas/TheStack.java
@@ -1,72 +1,46 @@
-import java.util.Random;
+package pilas;
+
 import java.util.Stack;
 
 public class TheStack<Type> implements TheStackInterface<Type> {
 
     private Stack<Type> _stack;
-
+    
     private Integer _capacity;
 
     public TheStack(Integer capacity) {
+        
         _stack = new Stack<Type>();
+        
         _capacity = capacity;
     }
 
     public Boolean push(Type item) {
-        var available = _stack.size() < _capacity;
-        if (available) {
-            push(item);
+        
+        if (_stack.size() < _capacity) {
+            _stack.push(item);
+            return true;
         }
-        return available;
+        return false;
     }
 
     public Type pop() {
-        return _stack.pop();
+        return _stack.isEmpty() ? null : _stack.pop();
     }
 
     public Type peek() {
-        return _stack.peek();
+        return _stack.isEmpty() ? null : _stack.peek();
     }
 
-    public boolean empty() {
+    public Boolean empty() {
         return _stack.isEmpty();
     }
 
-    public int size() {
+    public Integer size() {
         return _stack.size();
     }
 
     public String print() {
         return _stack.toString();
-    }
-
-    public static void main(String[] args) {
-        var capacity = Integer.parseInt(args[0]);
-        TheStackInterface<Double> stack = new TheStack<Double>(capacity);
-        System.out.println("Pushing {capacity + 1}");
-        var random = new Random();
-        for (var n = 0; n <= capacity; n++) {
-            var number = random.nextDouble();
-            var pushed = stack.push(number);
-            System.out.println(" ↳ push(" + number + ") → " + pushed);
-        }
-        System.out.println();
-        System.out.println("Pushed {full}");
-        System.out.println(" ↳ print() → " + stack.print());
-        System.out.println("   ↳ peek() → " + stack.peek());
-        System.out.println("   ↳ size() → " + stack.size());
-        System.out.println("   ↳ empty() → " + stack.empty());
-        System.out.println();
-        System.out.println("Popping {capacity + 1}");
-        for (var n = 0; n <= capacity; n++) {
-            var popped = stack.pop();
-            System.out.println(" ↳ pop() → " + popped);
-        }
-        System.out.println();
-        System.out.println("Popped {empty}");
-        System.out.println(" ↳ print() → " + stack.print());
-        System.out.println("   ↳ peek() → " + stack.peek());
-        System.out.println("   ↳ size() → " + stack.size());
-        System.out.println("   ↳ empty() → " + stack.empty());
     }
 }
