2069fb8bc0a3907c04acd939395522b743747888
piero1205 <pjcarva12@gmail.com>
2025-11-03 16:55:58 -0600

    Define ListInterface for doubly circular list

--- PP3/ListInterface.java
+++ PP3/ListInterface.java
@@ -1,1 +1,24 @@
+package listdoublycircular;
 
+public interface ListInterface<T> {
+
+    void addFirst(T value);
+
+    void addLast(T value);
+
+    T removeFirst();
+
+    T removeLast();
+
+    T getFirst();
+
+    T getLast();
+
+    Boolean isEmpty();
+
+    Integer getSize();
+
+    void printList();
+
+    void printListInReverse();
+}
