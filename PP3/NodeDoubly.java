3a820b64eab4b4921901b36959ae03ccb875319f
piero1205 <pjcarva12@gmail.com>
2025-11-03 16:54:57 -0600

    Add NodeDoubly class for doubly linked list

--- PP3/NodeDoubly.java
+++ PP3/NodeDoubly.java
@@ -1,1 +1,13 @@
+package listdoublycircular;
 
+public class NodeDoubly<T> {
+    T data; 
+    NodeDoubly<T> next; 
+    NodeDoubly<T> prev; 
+
+    public NodeDoubly(T value) {
+        data = value;
+        next = null;
+        prev = null;
+    }
+}
